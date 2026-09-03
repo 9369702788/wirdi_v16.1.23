603ba2101bdc9b0a30fe3f6e39f9607a2a6b296e
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-09-03 11:55:12 +0300

    Add files via upload

---
+++ lib/core/services/achievement_service.dart
@@ -0,0 +1,49 @@
+import 'sajda_tracker_service.dart';
+import 'user_progress_service.dart';
+
+class AchievementStats {
+  final int longestStreak;
+  final double quranRatio;
+  final int khatmasCompleted;
+  final int lifetimePages;
+  final int lifetimeAzkar;
+  final int lifetimeTasbeeh;
+  final int lifetimePrayers;
+  final int favoritesCount;
+  final int lifetimeSajdaCount;
+
+  const AchievementStats({
+    required this.longestStreak,
+    required this.quranRatio,
+    required this.khatmasCompleted,
+    required this.lifetimePages,
+    required this.lifetimeAzkar,
+    required this.lifetimeTasbeeh,
+    required this.lifetimePrayers,
+    required this.favoritesCount,
+    required this.lifetimeSajdaCount,
+  });
+
+  static Future<AchievementStats> load() async {
+    final longest = await UserProgressService.longestStreak();
+    final ratio = await UserProgressService.quranCompletionRatio();
+    final khatmas = await UserProgressService.khatmasCompletedCount();
+    final pages = await UserProgressService.lifetimePagesTotal();
+    final azkar = await UserProgressService.lifetimeAzkarTotal();
+    final tasbeeh = await UserProgressService.lifetimeTasbeehTotal();
+    final prayers = await UserProgressService.lifetimePrayersTotal();
+    final favorites = await UserProgressService.totalFavoritesCount();
+    final sajdaCount = await SajdaTrackerService.totalCount();
+    return AchievementStats(
+      longestStreak: longest,
+      quranRatio: ratio,
+      khatmasCompleted: khatmas,
+      lifetimePages: pages,
+      lifetimeAzkar: azkar,
+      lifetimeTasbeeh: tasbeeh,
+      lifetimePrayers: prayers,
+      favoritesCount: favorites,
+      lifetimeSajdaCount: sajdaCount,
+    );
+  }
+}
